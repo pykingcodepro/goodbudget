4850b5319f8fb2908752f35305a0f5cc733036e7
Vatsal Sejpal <sejpalvatsal456@gmail.com>
2025-10-10 19:26:58 +0530

    fixed prod error - any type error

--- src/components/BootstrapClient.tsx
+++ src/components/BootstrapClient.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 export default function BootstrapClient() {
   useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.min.js' as any); // Import Bootstrap's JS bundle
+    import('bootstrap/dist/js/bootstrap.bundle.min.js' as string); // Import Bootstrap's JS bundle
   }, []);
   return null;
 }
