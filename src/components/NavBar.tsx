8fba12fe1a6e095c7dcd8a26c185422393903666
Vatsal Sejpal <sejpalvatsal456@gmail.com>
2025-10-10 19:24:54 +0530

    fixed nav error

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,26 @@
+import Link from "next/link";
+
 export default function NavBarComponent() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-primary">
             <div className="container-fluid">
-                <a className="navbar-brand me-5" href="#">GoodBudget</a>
+                <Link className="navbar-brand me-5" href="#">GoodBudget</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item me-4">
-                            <a className="nav-link active" aria-current="page" href="/">Dashboard</a>
+                            <Link className="nav-link active" aria-current="page" href="/">Dashboard</Link>
                         </li>
                         <li className="nav-item me-4">
-                            <a className="nav-link" href="/analysis">Analysis</a>
+                            <Link className="nav-link" href="/analysis">Analysis</Link>
                         </li>
                         <li className="nav-item me-4">
-                            <a className="nav-link" href="/categories">Categories</a>
+                            <Link className="nav-link" href="/categories">Categories</Link>
                         </li>
                         <li className="nav-item me-4">
-                            <a className="nav-link" href="/settings">Settings</a>
+                            <Link className="nav-link" href="/settings">Settings</Link>
                         </li>
                     </ul>
                 </div>
