6277ea4c10d2f7438939b58c386b9013233bcb15
Vatsal Sejpal <sejpalvatsal456@gmail.com>
2025-10-11 13:55:24 +0530

    Changed NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBarComponent() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-primary mb-5">
             <div className="container-fluid">
-                <Link className="navbar-brand me-5" href="#">GoodBudget</Link>
+                <Link className="navbar-brand me-5" href="/">GoodBudget</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
